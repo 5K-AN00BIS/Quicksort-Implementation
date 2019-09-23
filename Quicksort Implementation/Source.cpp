ad48e679fb2649f1d2194317002affce4cc0414c
RYAN BACHMAN <RYABACH1@uat.edu>
2019-09-23 11:25:16 -0700

    Adding to source.cpp

--- Quicksort Implementation/Source.cpp
+++ Quicksort Implementation/Source.cpp
@@ -110,6 +110,53 @@ void quicksort(std::vector<Comparable>& a, int left, int right)
 	}
 }
 
+// Internal selection method that makes recursive calls. Uses median-of-three partitioning and a cutoff of 10.
+// Places the kth smallest item in a[k - 1].
+// a is an array of Comparable items. left is the left-most index. right is the right-most index. k is the desired rank (1 minimum) in the entire array.
+template <typename Comparable>
+void quickSelect(std::vector<Comparable>& a, int left, int right, int k)
+{
+	if (left + 10 <= right)
+	{
+		const Comparable& pivot = median3(a, left, right);
+
+		// Begin partitioning
+		int i = left, j = right - 1;
+
+		for (; ; )
+		{
+			while (a[++i] < pivot) {}
+
+			while (pivot < a[--j]) {}
+
+			if (i < j)
+			{
+				std::swap(a[i], a[j]);
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		std::swap(a[i], a[right - 1]); // Restore pivot
+
+		// Recurse; only this part changes.
+		if (k <= i)
+		{
+			quickSelect(a, left, i - 1, k);
+		}
+		else if (k > i + 1)
+		{
+			quickSelect(a, i + 1, right, k);
+		}
+	}
+	else // Do an insertion sort on the subarray.
+	{
+		insertionSort(a, left, right);
+	}
+}
+
 int main()
 {
 	return 0;
